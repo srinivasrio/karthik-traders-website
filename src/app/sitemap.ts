cfdb3cab071e78be210118a6d98710bf1d5675e2
srinivasrio <srinivasrio06@gmail.com>
2026-02-01 00:57:46 +0530

    SEO: Fixed domain TLD from .com to .in

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://karthiktraders.com'
+    const baseUrl = 'https://karthiktraders.in'
 
     // Static pages
     const routes = [
